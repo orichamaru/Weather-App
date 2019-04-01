e80afda0cc0e856ac56f6b696d4cc6113cc10a5a
Javed Ali <46164236+orichamaru@users.noreply.github.com>
2019-04-01 16:54:12 +0530

    Upload weather app files

---
+++ weatherapp/index.js
@@ -0,0 +1,60 @@
+//A common use of JSON is to read data from a web server, and display the data in a web page.
+
+//use the JavaScript built-in function JSON.parse() to convert the string into a JavaScript object
+
+const appKey = "151d19cd2b62977811e6201b5fc376b0";
+
+let searchButton = document.getElementById("search-btn");
+let searchInput =  document.getElementById("search-txt");
+let cityName = document.getElementById("city-name");
+let icon = document.getElementById("icon");
+let temperature = document.getElementById("temp");
+let humidity = document.getElementById("humidity-div");
+
+searchButton.addEventListener("click" , findWeatherDetails);
+searchInput.addEventListener("keyup" , enterPressed);
+
+function enterPressed(event)
+{
+	if(event.key == "Enter")
+	{
+		findWeatherDetails();
+	}
+}
+
+function findWeatherDetails()
+{
+	if(searchInput.value == "")
+	{
+	
+	}
+	else
+	{
+		let searchLink = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput.value + "&appid="+appKey;
+	    httpRequestAsync(searchLink, theResponse);
+	}
+}
+
+function theResponse(response)
+{
+	let jsonObject = JSON.parse(response) ;
+    cityName.innerHTML = jsonObject.name;
+    icon.src = "http://openweathermap.org/img/w/" + jsonObject.weather[0].icon + ".png";
+    temperature.innerHTML = parseInt(jsonObject.main.temp - 273) + "°";
+    humidity.innerHTML = jsonObject.main.humidity + "%";
+} 
+
+
+function httpRequestAsync(url , callback)
+{
+	console.log("hello");
+	var httpRequest = new XMLHttpRequest();
+	httpRequest.onreadystatechange = () =>
+    {
+    	if(httpRequest.readyState == 4 && httpRequest.status == 200)
+          callback(httpRequest.responseText);
+    }
+    httpRequest.open("GET", url, true); // true for asynchronous 
+    httpRequest.send(); 
+}
+
